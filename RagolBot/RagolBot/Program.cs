13fd45b39fafa14114917c07091cb2117562cbf9
Ragolsnagol <ragolsnagol@gmail.com>
2017-07-26 11:05:24 -0400

    Basic Ping pong bot made.

---
+++ RagolBot/RagolBot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+
+namespace RagolBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+
+            string token = "";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            //Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+    }
+}
